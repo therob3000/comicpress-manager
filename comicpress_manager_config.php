ff17423aa9fa5e31dc34968d67c3cf721d6587df
johncoswell <johncoswell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-04 01:02:08 +0000

    tagging 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/comicpress-manager/tags/1.4.1@91759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comicpress_manager_config.php
+++ comicpress_manager_config.php
@@ -10,25 +10,13 @@ define("CPM_SCALE_NONE", 0);
 define("CPM_SCALE_IMAGEMAGICK", 1);
 define("CPM_SCALE_GD", 2);
 
-// if you know what you're doing, you can improve performance by setting this option to true
-define("CPM_SKIP_CHECKS", false);
+$result = get_option('comicpress-manager-cpm-date-format');
+if (!empty($result)) {
+  define("CPM_DATE_FORMAT", $result);
+} else {
+  define("CPM_DATE_FORMAT", "Y-m-d");
+}
 
-// if you've hacked on ComicPress to support a different date format, change it here
-define("CPM_DATE_FORMAT", "Y-m-d");
-
-// if you want the older ComicPress Manager warnings, set this to false
-define("CPM_WP_STYLE_WARNINGS", true);
-
-// if you don't want to check uploaded files against GD to see if they're valid images,
-// set this to false
-define("CPM_DO_GD_FILETYPE_CHECKS", true);
-
-// if you don't want to see the ComicPress News Dashboard widget, set this to false
-define("CPM_SHOW_DASHBOARD_RSS_FEED", true);
-
-// if you need ot use a different level of permissions when uploading/modifying
-// files, change it here
-//
 // for Windows users, your permissions will automatically be set to 0777 (writable).
 // there is no easy way in PHP to modify permissions on an NTFS filesystem (and no
 // permissions to speak of on FAT32!)
@@ -37,33 +25,32 @@ if (strpos(PHP_OS, "WIN") !== false) {
   // for Windows users
   define("CPM_FILE_UPLOAD_CHMOD", 0777);
 } else {
-  // for Unix (Linux, Mac OS X, *BSD) users
-  // define("CPM_FILE_UPLOAD_CHMOD", 0644); // writable by webserver process only      (rare)
-  define("CPM_FILE_UPLOAD_CHMOD", 0664);    // writable by owner and any group members (common)
-  // define("CPM_FILE_UPLOAD_CHMOD", 0666); // writable by anyone                      (rare)
+  $result = get_option('comicpress-manager-cpm-upload-permissions');
+  $chmod_to_use = 0664; // writable by owner and any group members (common)
+  if (!empty($result)) {
+    $requested_chmod_to_use = 0;
+    $strlen_result = strlen($result);
+    $is_ok = true;
+    for ($i = 0; $i < $strlen_result; ++$i) {
+      $char = $result{$strlen_result - 1 - $i};
+      if (preg_match('#[0-7]#', $char) > 0) {
+        $requested_chmod_to_use = $requested_chmod_to_use + ($char * pow(8, $i));
+      } else {
+        $is_ok = false;
+      }
+    }
+    if ($is_ok) {
+      $chmod_to_use = $requested_chmod_to_use;
+    }
+  }
+  define("CPM_FILE_UPLOAD_CHMOD", $chmod_to_use);
 }
 
-$cpm_config_properties = array(
-  // change these to something you like better...
-
-  'default_post_time' => "12:00 am",
-  'default_post_content' => "",
-  'default_override_title' => '',
-  'default_post_tags' => "",
-  'default_additional_categories' => array(12),
-  'archive_generate_thumbnails' => true,
-  'rss_generate_thumbnails'     => true,
-  'thumbnail_quality'           => 80
-);
-
 // CPM_DOCUMENT_ROOT override
-//
-// If you are having issues with your comics folders or your WordPress site root
-// not being found, then you will need to override the auto-calaculated
-// CPM_DOCUMENT_ROOT with the absolute path to your site root (where the WordPress
-// index.php is located, and most likely the same location as your comics folders).
-// Please make sure there is no / at the end of the path!
 
-// define('CPM_DOCUMENT_ROOT', '/your/very/nonstandard/setup/path')
+$result = get_option("comicpress-manager-cpm-document-root");
+if (!empty($result)) {
+  define('CPM_DOCUMENT_ROOT', untrailingslashit($result));
+}
 
 ?>
--- pages/comicpress_dates.php
+++ pages/comicpress_dates.php
@@ -119,7 +119,9 @@ function cpm_manager_dates() {
           });
         }
       </script>
-      <input type="submit" id="submit" value="<?php _e("Change Dates", 'comicpress-manager') ?>" style="width: 520px" />
+      <div style="text-align: center">
+        <input class="button" type="submit" id="submit" value="<?php _e("Change Dates", 'comicpress-manager') ?>" />
+      </div>
     </form>
   <?php } else { ?>
     <p><?php _e("You haven't uploaded any comics yet.", 'comicpress-manager') ?></p>
